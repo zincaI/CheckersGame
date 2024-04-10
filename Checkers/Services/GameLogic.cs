df36bd28805fdd1a4e8c75c2bb7287f76bcdba43
zincaI <zinca.irina13@gmail.com>
2024-04-10 09:08:35 +0300

    Added classes for base notifications, a piece and game logic

--- Checkers/Services/GameLogic.cs
+++ Checkers/Services/GameLogic.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Checkers.Models;
+//using Checkers.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,19 @@ namespace Checkers.Services
 {
     internal class GameLogic
     {
+        private ObservableCollection<ObservableCollection<Piece>> Board;
+
+        List<int> toColor = new List<int> { };
+
+        public GameLogic()
+        {
+        }
+
+        public GameLogic(ObservableCollection<ObservableCollection<Piece>> pieces)
+        {
+            this.Board = pieces;
+        }
+
+
     }
 }
